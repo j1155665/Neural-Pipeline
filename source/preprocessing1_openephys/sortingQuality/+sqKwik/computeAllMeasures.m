8c4ae24ff05c0c20607b10d9b44f600e757559c1
Yueh-Chen Chiang <122631423+j1155665@users.noreply.github.com>
2026-02-03 14:40:10 -0500

    Delete source directory

--- source/preprocessing1_openephys/sortingQuality/+sqKwik/computeAllMeasures.m
+++
@@ -1,9 +0,0 @@
-
-
-function [cgs, uQ, cR, isiV] = computeAllMeasures(baseFilename, shankIndex)
-
-cgs = readKwikCGs(baseFilename, shankIndex);
-
-[cids, uQ, cR] = sqKwik.maskedClusterQuality(baseFilename, shankIndex);
-
-isiV = sqKwik.isiViolations(baseFilename, shankIndex);
